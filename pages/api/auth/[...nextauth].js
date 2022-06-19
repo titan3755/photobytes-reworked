bbec32254422e37c6072403ca979656457d8c352
Titan <68808479+titan3755@users.noreply.github.com>
2022-06-19 13:12:10 +0600

    Login Fixes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ export default NextAuth({
                 password: {  label: "Password", type: "password" }
             },
             async authorize(credentials, req) {
-                const res = await fetch('http://localhost:3000/api/login', {
+                const res = await fetch(process.env.WEBSITE_DOM, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
