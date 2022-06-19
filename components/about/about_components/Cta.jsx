e51a3b76881c6e5aa327af8785e6c1e62f412f42
Titan <68808479+titan3755@users.noreply.github.com>
2022-06-19 12:40:02 +0600

    Build error fixes

--- components/about/about_components/Cta.jsx
+++ components/about/about_components/Cta.jsx
@@ -10,8 +10,8 @@ export default function Cta () {
                     </div>
                     <div className="container bg-slate-700 w-full xl:w-[50%] h-full p-10 flex flex-col justify-center rounded-b-lg xl:rounded-b-none xl:rounded-r-lg">
                         <h1 className="text-slate-400 mx-auto sm:mx-0 text-base font-semibold my-2">BLAZING FAST RESPONSES</h1>
-                        <h1 className="text-white mx-auto sm:mx-0 text-4xl text-center sm:text-left font-extrabold my-2">We're here to help</h1>
-                        <p className="text-slate-300 mx-auto sm:mx-0 text-center sm:text-left text-base font-normal my-2 w-auto sm:w-[500px]">Facing any problems with our services or do you want to provide feedback or complaints? Contact us directly by Facebook Messenger or via other platforms to let us know of your problems! We'll make sure that you are satisfied with our services and your feedback is greatly appreciated.</p>
+                        <h1 className="text-white mx-auto sm:mx-0 text-4xl text-center sm:text-left font-extrabold my-2">We&apos;re here to help</h1>
+                        <p className="text-slate-300 mx-auto sm:mx-0 text-center sm:text-left text-base font-normal my-2 w-auto sm:w-[500px]">Facing any problems with our services or do you want to provide feedback or complaints? Contact us directly by Facebook Messenger or via other platforms to let us know of your problems! We&apos;ll make sure that you are satisfied with our services and your feedback is greatly appreciated.</p>
                         <a className="p-4 mx-auto sm:mx-0 w-[200px] bg-white text-black font-medium text-lg rounded-lg mt-7 text-center hover:scale-[96%] transition-transform duration-200" href="">Customer Support</a>
                     </div>
                 </div>
--- components/home/home_components/Benefits.jsx
+++ components/home/home_components/Benefits.jsx
@@ -4,7 +4,7 @@ export default function Benefits () {
     return ( 
         <Fragment>
             <div className="container bg-gradient-to-br from-[#FFAFBD] to-[#ffc3a0] flex flex-col align-middle justify-center min-w-full h-[550px]">
-                <h1 className="text-center mx-auto text-4xl md:text-7xl md:leading-[5.5rem] font-sans font-bold md:font-extrabold text-white" style={{textShadow: "0 0 4px #000000, 0 0 4px #000000"}}>You'll get the best services for the best prices at PhotoBytes Studios</h1>
+                <h1 className="text-center mx-auto text-4xl md:text-7xl md:leading-[5.5rem] font-sans font-bold md:font-extrabold text-white" style={{textShadow: "0 0 4px #000000, 0 0 4px #000000"}}>You&apos;ll get the best services for the best prices at PhotoBytes Studios</h1>
             </div>
         </Fragment>
     )
