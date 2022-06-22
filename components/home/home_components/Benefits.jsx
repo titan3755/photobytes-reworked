1c9ee4423d5339e62f3bf8e9c23b855301727cc9
Titan <68808479+titan3755@users.noreply.github.com>
2022-06-22 17:43:59 +0600

    Code Correction

--- components/home/home_components/Benefits.jsx
+++ components/home/home_components/Benefits.jsx
@@ -23,7 +23,7 @@ export default function Benefits () {
                     carouselData.map(unit => {
                         return (
                             <SwiperSlide key={unit.title}>
-                                <img src={unit.imgUrl} alt="" srcset="" className="w-full object-cover brightness-[40%]" />
+                                <img src={unit.imgUrl} alt="img not available" className="w-full object-cover brightness-[40%]" />
                                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                     <h2 className="text-center text-white font-bold text-5xl">{unit.title}</h2>
                                     <p className="mt-6 text-white font-sans font-medium text-lg">{unit.desc}</p>
